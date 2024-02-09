330766d60644729e90b7779d0be9ff1c2ebc4c13
Helmet Pikkel <helmet.pikkel@gmail.com>
2024-02-09 09:40:28 +0200

    add dev server and .gitkeep

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+      },
+    plugins: [new HtmlWebpackPlugin()],
+};
